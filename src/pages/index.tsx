557671eb25e9dfed3e0bd89438bcefb9741b3b80
nst23 <nst23@txstate.edu>
2023-05-09 23:03:53 -0500

    added contact page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,6 +97,67 @@ export default function Home() {
             </div>
           </div>
         </div>
+        {/* <div>
+          <h1>Get in touch</h1>
+          <form>
+            <h2>Contact</h2>
+            <h3>Your Name</h3>
+            <input></input>
+            <h3>Email</h3>
+            <input></input>
+            <h3>Phone</h3>
+            <h3>Message</h3>
+            <textarea></textarea>
+          </form>
+        </div> */}
+        <div>
+          <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center">
+              Contact
+            </h2>
+            <form action="#" className="space-y-8">
+            <div>
+                <label
+                  htmlFor="name"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Name
+                </label>
+                <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Neo" required />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  Your email
+                </label>
+                <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@matrix.com" required />
+              </div>
+              
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                >
+                  Your message
+                </label>
+                <textarea
+                  id="message"
+                  rows={6}
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Leave a message..."
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="py-3 px-5 text-sm font-medium text-center rounded-lg sm:w-fit bg-gradient-to-r from-blue-500 to-green-500 text-white"
+              >
+                Send message
+              </button>
+            </form>
+          </div>
+        </div>
       </section>
     </main>
   );

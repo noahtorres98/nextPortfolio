659b4bc9abfa85e3ee7c9ccdaa8e49c249bd9657
nst23 <nst23@txstate.edu>
2023-05-23 15:00:21 -0500

    added netlify form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -171,7 +171,7 @@ export default function Home() {
               <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center dark:text-white">
                 Contact
               </h2>
-              <form onSubmit={handleSubmit} className="space-y-8">
+              <form name="contact" onSubmit={handleSubmit} className="space-y-8" method="POST" data-netlify="true">
                 <div>
                   <label
                     htmlFor="name"
@@ -181,6 +181,7 @@ export default function Home() {
                   </label>
                   <input
                     type="text"
+                    name="name"
                     id="name"
                     className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                     placeholder="Neo"
@@ -196,6 +197,7 @@ export default function Home() {
                   </label>
                   <input
                     type="email"
+                    name="email"
                     id="email"
                     className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                     placeholder="name@matrix.com"
@@ -211,6 +213,7 @@ export default function Home() {
                   </label>
                   <textarea
                     id="message"
+                    name="message"
                     rows={6}
                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     placeholder="Leave a message..."

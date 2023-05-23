6a31874965533cfec22909743ede4ee03928a87b
nst23 <nst23@txstate.edu>
2023-05-23 15:42:01 -0500

    fixed resume button for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
                 <CiDark className="dark:text-white" />
               </div>
               <li>
-                <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                <button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 rounded-md">
                   <a
                     className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                     href="/Torres Resume 2023.pdf"

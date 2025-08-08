8e2e79591ba7da6ba8b820c6a5d0706e5fc0a68c
Noah Torres <noahstores98@gmail.com>
2025-08-07 22:01:51 -0500

    Fixing issue.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,19 @@
-import '@component/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@component/styles/globals.css";
+import type { AppProps } from "next/app";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from 'react';
-import { Montserrat } from "@next/font/google";
+import { useEffect } from "react";
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
 });
 
-
-
-
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init();
-  })
+  });
   return (
     <div className={montserrat.className}>
       <Component {...pageProps} />

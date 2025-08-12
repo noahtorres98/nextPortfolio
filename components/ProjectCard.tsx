2dc7b9c167f18e21883b58d92dabd3bad69ea3de
Noah Torres <noahstores98@gmail.com>
2025-08-11 22:44:23 -0500

    Fixed image styling.

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -9,19 +9,24 @@ interface ProjectCardProps {
 
 function ProjectCard({ imgSrc, description, flip, link }: ProjectCardProps) {
   const content = (
-    <>
-      <div className="relative w-full h-80 rounded overflow-hidden">
-        <Image src={imgSrc} alt={description} fill className="rounded" />
-      </div>
-      <p className="text-sm lg:text-lg shadow-lg p-5 text-white rounded dark:bg-cyan-800 absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-cyan-500 to-teal-500 opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+    <div className="relative w-full aspect-video rounded overflow-hidden">
+      <Image
+        src={imgSrc}
+        alt={description}
+        fill
+        className="object-fill"
+        sizes="(max-width: 768px) 100vw, 50vw"
+      />
+
+      <p className="absolute inset-0 flex items-center justify-center text-center text-sm lg:text-lg shadow-lg text-white rounded dark:bg-cyan-800 bg-gradient-to-r from-cyan-500 to-teal-500 opacity-0 transition duration-300 ease-in-out hover:opacity-100 p-5">
         {description}
       </p>
-    </>
+    </div>
   );
 
   return (
     <div
-      className="relative shadow-xl"
+      className="shadow-xl"
       data-aos={`flip-${flip}`}
       data-aos-duration="2000"
     >
